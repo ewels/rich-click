a09438af57f2369d185f3030d0bea512d90e26bb
Phil Ewels <phil.ewels@scilifelab.se>
2022-02-10 09:41:57 +0100

    Bump version to 0.1.2

--- src/rich_click/__init__.py
+++ src/rich_click/__init__.py
@@ -6,6 +6,6 @@ The intention is to provide attractive help output from
 click, formatted with rich, with minimal customisation required.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .core import rich_format_help
