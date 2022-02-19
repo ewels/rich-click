6409e1f33103a9a6e7153de3f49ecf36538fd7a3
Timothée Mazzucotelli <pawamoy@pm.me>
2022-02-19 13:58:15 +0100

    Allow to run any Click program from the CLI

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 setup(
     name="rich-click",
+    entry_points={"console_scripts": ["rich-click = rich_click.cli:main"]},
     install_requires=[
         "click",
         "rich",
+        "importlib-metadata; python_version < '3.8'",
     ],
 )
---
+++ src/rich_click/cli.py
@@ -0,0 +1,46 @@
+"""The command line interface."""
+
+import sys
+from importlib import import_module
+try:
+    from importlib.metadata import entry_points
+except ImportError:
+    from importlib_metadata import entry_points
+
+import click
+from rich_click import group, command
+
+
+def main(args=None):
+    args = args or sys.argv[1:]
+    if not args:
+        # without args we assume we want to run rich-click on itself
+        # TODO: rewrite using argparse
+        script_name = "rich-click"
+    else:
+        script_name = args[0]
+    scripts = {script.name: script for script in entry_points().get("console_scripts")}
+    if script_name in scripts:
+        # a valid script was passed
+        script = scripts[script_name]
+        module_path = script.module
+        function_name = script.attr
+        prog = script_name
+    elif ":" in script_name:
+        # the path to a function was passed
+        module_path, function_name = args[0].split(":")
+        prog = module_path.split(".", 1)[0]
+    else:
+        print("usage: rich-click [SCRIPT | MODULE:FUNCTION] [-- SCRIPT_ARGS...]", file=sys.stderr)
+        sys.exit(1)
+    if len(args) > 1:
+        if args[1] == "--":
+            del args[1]
+    # patch click before importing the program function
+    click.group = group
+    click.command = command
+    # import the program function
+    module = import_module(module_path)
+    function = getattr(module, function_name)
+    # simply run it: it should be patched as well
+    return function(*args[1:])
