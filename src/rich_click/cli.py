c0a18ca285bf28d6906675de94c71c33d1d7a710
dwreeves <xdanielreeves@gmail.com>
2022-03-13 20:57:14 -0400

    Override the class in addition to the decorator

--- src/rich_click/cli.py
+++ src/rich_click/cli.py
@@ -19,6 +19,8 @@ from rich.text import Text
 
 from rich_click import command as rich_command
 from rich_click import group as rich_group
+from rich_click import RichCommand
+from rich_click import RichGroup
 from rich_click.rich_click import (
     ALIGN_ERRORS_PANEL,
     ERRORS_PANEL_TITLE,
@@ -123,6 +125,8 @@ def main(args: Optional[List[str]] = None) -> Any:
     # patch click before importing the program function
     click.group = rich_group
     click.command = rich_command
+    click.Group = RichGroup
+    click.Command = RichCommand
     # import the program function
     module = import_module(module_path)
     function = getattr(module, function_name)
