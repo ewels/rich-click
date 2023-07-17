25ba3764904ea10638fe9c1c3d22e2ad11f91749
dwreeves <xdanielreeves@gmail.com>
2023-07-16 22:17:32 -0400

    deprecate click 7.x support

--- src/rich_click/_compat_click.py
+++ src/rich_click/_compat_click.py
@@ -1,5 +1,17 @@
-from distutils.version import LooseVersion
+import warnings
 
 import click
 
-CLICK_IS_BEFORE_VERSION_8X = LooseVersion(click.__version__) < LooseVersion("8.0.0")
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=DeprecationWarning)
+    # TODO: Remove use of distutils.version.LooseVersion (distutils.version is deprecated).
+    from distutils.version import LooseVersion
+
+    CLICK_IS_BEFORE_VERSION_8X = LooseVersion(click.__version__) < LooseVersion("8.0.0")
+
+if CLICK_IS_BEFORE_VERSION_8X:
+    warnings.warn(
+        "rich-click support for click 7.x is deprecated and will be removed soon."
+        " Please upgrade click to a newer version.",
+        DeprecationWarning,
+    )
