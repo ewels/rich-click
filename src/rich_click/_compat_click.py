0b4ab2a2870be3d89c4351d4bd99d91ed1a3c8ff
dwreeves <xdanielreeves@gmail.com>
2023-10-02 13:51:19 -0400

    pass pre-commit

--- src/rich_click/_compat_click.py
+++ src/rich_click/_compat_click.py
@@ -1,13 +1,13 @@
 import click
 from packaging import version
 
-
 CLICK_IS_BEFORE_VERSION_8X = version.parse(click.__version__) < version.parse("8.0.0")
 CLICK_IS_VERSION_80 = version.parse("8.1.0") > version.parse(click.__version__) >= version.parse("8.0.0")
 
 
 if CLICK_IS_BEFORE_VERSION_8X:
     import warnings
+
     warnings.warn(
         "rich-click support for click 7.x is deprecated and will be removed soon."
         " Please upgrade click to a newer version.",
